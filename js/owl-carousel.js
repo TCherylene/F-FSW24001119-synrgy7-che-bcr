69bc974cb1e5b9168e6804653952cce68b736c1f
Chery <cherylene40@gmail.com>
2024-03-11 18:15:08 +0700

    add owl carousel

---
+++ js/owl-carousel.js
@@ -0,0 +1,26 @@
+// Javascript owl carousel configuration reference:
+// https://bbbootstrap.com/snippets/bootstrap-4-owl-carousel-for-user-testimonials-10735496
+
+$(document).ready(function () {
+    var slider = $(".owl-carousel");
+    slider.owlCarousel({
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
+        items: 1,
+        stagePadding: 20,
+        center: true,
+        nav: true,
+        margin: 50,
+        dots: false,
+        loop: true,
+        responsive: {
+            0: { items: 1 },
+            480: { items: 1 },
+            575: { items: 1 },
+            768: { items: 2 },
+            991: { items: 2 },
+            1200: { items: 2 }
+        }
+    });
+});
