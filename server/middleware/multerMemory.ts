c1af66d3d37660b0f36e6a82fb4ddea2fafc8984
Chery <cherylene40@gmail.com>
2024-06-02 16:49:38 +0700

    cleaning unused file

--- server/middleware/multerMemory.ts
+++
@@ -1,9 +0,0 @@
-import multer from 'multer';
-import path from 'path';
-
-const storage = multer.memoryStorage()
-
-const multerInstance = multer({ storage });
-export {
-    multerInstance as uploadOnMemory
-}
