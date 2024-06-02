8024e5ff092796a86a49980ddfcea7ccef158305
Chery <cherylene40@gmail.com>
2024-06-02 16:50:40 +0700

    update database configuration

--- knexfile.ts
+++ knexfile.ts
@@ -1,6 +1,7 @@
 import type { Knex } from "knex";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
+
 // Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
@@ -9,8 +10,20 @@ const config: { [key: string]: Knex.Config } = {
     connection: {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      port: Number(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432')
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./db/migrations"
+    },
+    seeds: {
+      directory: "./db/seeds"
     }
   },
 
@@ -45,8 +58,6 @@ const config: { [key: string]: Knex.Config } = {
       tableName: "knex_migrations"
     }
   }
-
 };
 
-export { config };
 module.exports = config;
